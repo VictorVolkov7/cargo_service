1bfb39b2f7d0a876e52b8ceac92a9e8fbcddf4e5
Victor Volkov <vektorn1212@gmail.com>
2024-03-29 02:36:28 +0200

    added resource class for Location model to fill in data

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from import_export import resources
 
-# Register your models here.
+from locations.models import Location
+
+
+class LocationResource(resources.ModelResource):
+    """
+    Resource (django-import-export) for Location model
+    to fill in data from a csv file.
+    """
+
+    class Meta:
+        model = Location
+
+
+# register location model in django admin
+admin.site.register(Location)
